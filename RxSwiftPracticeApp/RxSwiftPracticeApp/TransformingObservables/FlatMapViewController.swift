f36d6f19fa43f7c146f91da79c082e80128660d0
siheo <shintwl@icloud.com>
2024-01-16 15:04:46 +0900

    FlatMap 사용 예시 변경

--- RxSwiftPracticeApp/RxSwiftPracticeApp/TransformingObservables/FlatMapViewController.swift
+++ RxSwiftPracticeApp/RxSwiftPracticeApp/TransformingObservables/FlatMapViewController.swift
@@ -12,7 +12,7 @@ class FlatMapViewController: UIViewController {
 
     private var descLabel: UILabel = {
         let label = UILabel()
-        label.text = "숫자값과 알파벳 값을 조합합니다"
+        label.text = "숫자를 터치하면 알파벳 ABC가 출력됩니다"
         label.font = .systemFont(ofSize: 18)
         label.numberOfLines = 1
         label.textColor = .black
@@ -20,14 +20,6 @@ class FlatMapViewController: UIViewController {
     }()
     
     private var numbereButtonStackView: UIStackView = {
-        let view = UIStackView()
-        view.axis = .horizontal
-        view.distribution = .fillEqually
-        view.spacing = 10
-        return view
-    }()
-    
-    private var alphabetButtonStackView: UIStackView = {
         let view = UIStackView()
         view.axis = .horizontal
         view.distribution = .fillEqually
@@ -75,36 +67,7 @@ class FlatMapViewController: UIViewController {
         return button
     }()
     
-    private var buttonA: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("A", for: .normal)
-        button.tintColor = .white
-        button.titleLabel?.font = .systemFont(ofSize: 24, weight: .bold)
-        button.backgroundColor = .systemBlue
-        button.tag = 1001
-        return button
-    }()
-    
-    private var buttonB: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("B", for: .normal)
-        button.tintColor = .white
-        button.titleLabel?.font = .systemFont(ofSize: 24, weight: .bold)
-        button.backgroundColor = .systemBlue
-        button.tag = 1002
-        return button
-    }()
-    
     private var numberButtonTouchHistoryLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 18)
-        label.text = "터치한 값 = "
-        label.numberOfLines = 0
-        label.tintColor = .black
-        return label
-    }()
-    
-    private var alphabetButtonTouchHistoryLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 18)
         label.text = "터치한 값 = "
@@ -124,7 +87,6 @@ class FlatMapViewController: UIViewController {
     
     private var bag = DisposeBag()
     private var numberObservable = PublishSubject<Int>()
-    private var alphabetObservable = PublishSubject<String>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -140,9 +102,7 @@ class FlatMapViewController: UIViewController {
         [
             self.descLabel,
             self.numbereButtonStackView,
-            self.alphabetButtonStackView,
             self.numberButtonTouchHistoryLabel,
-            self.alphabetButtonTouchHistoryLabel,
             self.resultLabel,
         ].forEach { self.view.addSubview($0) }
         
@@ -155,32 +115,19 @@ class FlatMapViewController: UIViewController {
             $0.addTarget(self, action: #selector(numberButtonTapped), for: .touchUpInside)
             self.numbereButtonStackView.addArrangedSubview($0)
         }
-        
-        [
-            self.buttonA,
-            self.buttonB,
-        ].forEach {
-            $0.addTarget(self, action: #selector(alphabetButtonTapped), for: .touchUpInside)
-            self.alphabetButtonStackView.addArrangedSubview($0)
-        }
     }
     
     private func configureAutoLayout() {
         [
             self.descLabel,
             self.numbereButtonStackView,
-            self.alphabetButtonStackView,
             self.numberButtonTouchHistoryLabel,
-            self.alphabetButtonTouchHistoryLabel,
             self.resultLabel,
             
             self.button1,
             self.button2,
             self.button3,
             self.button4,
-            
-            self.buttonA,
-            self.buttonB,
         ].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
         
         NSLayoutConstraint.activate([
@@ -197,17 +144,8 @@ class FlatMapViewController: UIViewController {
             self.numberButtonTouchHistoryLabel.topAnchor.constraint(equalTo: self.numbereButtonStackView.bottomAnchor, constant: 10),
             self.numberButtonTouchHistoryLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
             self.numberButtonTouchHistoryLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -15),
-
-            self.alphabetButtonStackView.topAnchor.constraint(equalTo: self.numberButtonTouchHistoryLabel.bottomAnchor, constant: 15),
-            self.alphabetButtonStackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
-            self.alphabetButtonStackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -15),
-            self.alphabetButtonStackView.heightAnchor.constraint(equalToConstant: 60),
             
-            self.alphabetButtonTouchHistoryLabel.topAnchor.constraint(equalTo: self.alphabetButtonStackView.bottomAnchor, constant: 10),
-            self.alphabetButtonTouchHistoryLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
-            self.alphabetButtonTouchHistoryLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -15),
-            
-            self.resultLabel.topAnchor.constraint(equalTo: self.alphabetButtonTouchHistoryLabel.bottomAnchor, constant: 10),
+            self.resultLabel.topAnchor.constraint(equalTo: self.numberButtonTouchHistoryLabel.bottomAnchor, constant: 10),
             self.resultLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
             self.resultLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -15),
         ])
@@ -215,54 +153,39 @@ class FlatMapViewController: UIViewController {
     
     private func configureDataBinding() {
         numberObservable
-            .flatMap{ [weak self] (number: Int) -> Observable<String> in
-                guard let self = self else {
-                    return Observable<String>.create { observer in
-                        observer.onNext("\(number)")
-                        observer.onCompleted()
-                        
-                        return Disposables.create()
+            .flatMap{ (number: Int) -> Observable<String> in
+                return Observable.create { observer in
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        observer.onNext("A")
                     }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        observer.onNext("B")
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        observer.onNext("C")
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                        observer.onCompleted()
+                    }
+                    
+                    return Disposables.create()
                 }
-                return self.alphabetObservable.map { "\($0)\(number)" }
             }
             .subscribe(
-                onNext: { [weak self] number in
+                onNext: { [weak self] str in
                     guard let self = self else { return }
                     
                     if self.resultLabel.text == nil {
-                        self.resultLabel.text = "\(number)"
+                        self.resultLabel.text = str
                     } else {
-                        self.resultLabel.text! += "\(number)"
+                        self.resultLabel.text! += str
                     }
                 }
             )
             .disposed(by: self.bag)
-    }
-    
-    @objc private func alphabetButtonTapped(_ button: UIButton) {
-        let tag = button.tag
-        
-        
-        switch tag {
-        case 1001:
-            if self.alphabetButtonTouchHistoryLabel.text == nil {
-                self.alphabetButtonTouchHistoryLabel.text = "터치한 값: A"
-            } else {
-                self.alphabetButtonTouchHistoryLabel.text! += "A"
-            }
-            
-            alphabetObservable.onNext("A")
-        case 1002:
-            if self.alphabetButtonTouchHistoryLabel.text == nil {
-                self.alphabetButtonTouchHistoryLabel.text = "터치한 값: B"
-            } else {
-                self.alphabetButtonTouchHistoryLabel.text! += "B"
-            }
-            
-            alphabetObservable.onNext("B")
-        default: break
-        }
     }
     
     @objc private func numberButtonTapped(_ button: UIButton) {
